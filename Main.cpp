cb9f302b9240ff35230426346eef89834f454e05
hulefei <hulefei29@gmail.com>
2021-12-27 23:07:12 +0800

    【add】添加tutorial02

--- Main.cpp
+++ Main.cpp
@@ -1,46 +1,18 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+﻿
+#include <cstdio>
+#include <cstdlib>
+#include "tutorial01_first_window/tutorial01.h"
+#include "tutorial02_red_triangle/tutorial02.h"
 
-int main() 
+int main(int argc,char *argv[])
 {
-	glfwInit();
+    printf("arg count = %d\n",argc);
+    printf("arg count = %s\n",argv[0]);
+    printf("arg count = %s\n",argv[1]);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // tutorial01();
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "HelloOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	gladLoadGL(); 
-
-	glViewport(0, 0, 800, 800);
-	
-
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-
-		// Take care of all GLFW events
-		glfwPollEvents();
-	}
-
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
-
-
-	return 0;
-}
+    tutorial02();
+    
+    return 0;
+} 
---
+++ tutorial02_red_triangle/tutorial02.h
@@ -0,0 +1,7 @@
+﻿// // Copyright 2020 Tencent Inc.  All rights reserved.
+// //
+// // Author: lefeihu@tencent.com (lefeihu)
+
+#pragma once
+
+int tutorial02();
